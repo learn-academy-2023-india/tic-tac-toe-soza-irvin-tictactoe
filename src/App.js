6572de4d9dacf34aa9bd250106b9c585f3763e4f
RobertSarsoza <sarsoza@gmail.com>
2023-12-14 15:13:19 -0800

    added stretch goals

--- src/App.js
+++ src/App.js
@@ -2,45 +2,66 @@ import React, { useState } from 'react'
 import Square from './components/Square'
 import './App.css'
 
-const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [player, setPlayer] = useState(true);
+  const calculateWinner = (board, currentPlayer) => {
+      const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
 
-  const handleClick = (index) => {
-  if (squares[index]) {
-    return
+  for (const win of winPatterns) {
+    const [a, b, c] = win
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return `${currentPlayer} wins!`
+    }
   }
-  const clickSquare = squares.slice()
 
- if (player) {
-    clickSquare[index] = "X"
-  } else { 
-    clickSquare[index] = "O"
+  if (board.every((spot) => spot !== null)) {
+    return 'Cats Game!! Play again?'
   }
-  setPlayer(!player);
-
-  setSquares(clickSquare)
+  return null
 }
 
-useEffect(() => {
-  console.log("squares:", squares);
-}, [squares])
+const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [currentPlayer, setCurrentPlayer] = useState('❌')
+  const [turnNotification, setTurnNotification] = useState("Player ❌'s turn")
 
-  // setSquares(clickSquare, () => {
-  //   console.log("squares:", squares)
-  // })
-  // setPlayer(!player)
-  // }
-  
-    return (
-      <>
-        <h1>Tic Tac Toe</h1>
-        <div className="game-board">
-          {squares.map((value, index) => ( 
-        <Square key={index} value={value} onClick={() => handleClick(index)}/>
-          ))}
-          </div>
-      </>
-    )
+const handleClick = (id) => {
+  if (board[id] === null) {
+    const newBoard = [...board]
+    newBoard[id] = currentPlayer
+    setBoard(newBoard)
+    setCurrentPlayer(currentPlayer === '❌' ? '⭕️' : '❌')
+    setTurnNotification(`Player ${currentPlayer === '❌' ? '⭕️' : '❌'}'s turn`)
   }
+  const winner = calculateWinner(board, currentPlayer)
+  if (winner){
+    alert(`Player ${winner} wins!`)
+  }
+}
+
+const restartGame = () => {
+  setBoard(Array(9).fill(null))
+  setCurrentPlayer('❌')
+  setTurnNotification("Player ❌'s turn")
+}
+
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Square board = {board} handleClick ={handleClick}/>
+      <h3>  {calculateWinner(board, currentPlayer)} </h3>
+      <p>{turnNotification}</p>
+      <button onClick = {restartGame}>Restart Game</button>
+    </>
+  )
+}
+
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,18 @@
-import React from 'react'
 
-const Square = (property) => {
+import React from "react"
+const Square = (props) => {
   return (
-    <div className="square"></div>
+    <div className="grid"> 
+    {props.board.map((value, index) => {
+      return(
+        <main key={index}>
+          <div className = "square" value={value} onClick={() => props.handleClick(index)}>
+              {value}
+            </div>
+            </main>
+          )
+        })}
+    </div>
   )
 }
 export default Square
