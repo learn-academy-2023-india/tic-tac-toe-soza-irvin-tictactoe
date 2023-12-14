87f2f9b00b554bf10c38ba3ca4c360dfc79672dd
RobertSarsoza <sarsoza@gmail.com>
2023-12-13 16:47:43 -0800

    stuck

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
-  
-  console.log("squares:", squares)
+  const [player, setPlayer] = useState(true);
 
+  const handleClick = (index) => {
+  if (squares[index]) {
+    return
+  }
+  const clickSquare = squares.slice()
+
+ if (player) {
+    clickSquare[index] = "X"
+  } else { 
+    clickSquare[index] = "O"
+  }
+  setPlayer(!player);
+
+  setSquares(clickSquare)
+}
+
+useEffect(() => {
+  console.log("squares:", squares);
+}, [squares])
+
+  // setSquares(clickSquare, () => {
+  //   console.log("squares:", squares)
+  // })
+  // setPlayer(!player)
+  // }
+  
     return (
       <>
         <h1>Tic Tac Toe</h1>
         <div className="game-board">
           {squares.map((value, index) => ( 
-        <Square key={index} value={value} />
+        <Square key={index} value={value} onClick={() => handleClick(index)}/>
           ))}
           </div>
       </>
